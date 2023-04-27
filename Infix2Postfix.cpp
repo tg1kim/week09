479b1de191061918363adabf78306a4a5448d5bf
tg1kim <ktg@kangnam.ac.kr>
2023-04-28 08:13:15 +0900

    Add files via upload

---
+++ Infix2Postfix.cpp
@@ -0,0 +1,108 @@
+#include <stdio.h>
+
+#define STACK_SIZE	40
+#define EXPR_SIZE	100
+#define ISP			1
+#define ICP			2
+#define MinOpr		'?'
+#define	EMPTY		0x20
+#pragma warning(disable: 4326 4996 6031)
+
+/** Create Stack **/
+typedef char Item;
+
+typedef struct {
+	int  nTop;
+	Item items[STACK_SIZE];
+}	Stack, *StackPtr;
+
+
+void main()
+{
+	int Infix2Postfix(char *sInfix);
+
+	while (1) {
+		char sInfix[EXPR_SIZE];
+		printf("\n  Infix Expr ? ");
+		gets_s(sInfix);
+		if (*sInfix == NULL)
+			return;
+		printf("Postfix Expr : ");
+		Infix2Postfix(sInfix);
+	}
+}
+
+int Infix2Postfix(char *sInfix)
+{
+	int push(StackPtr pStack, Item op);
+	Item pop(StackPtr pStack);
+	Item top(StackPtr pStack);
+	int priority(char op, int mode);
+	int printStackOpr(StackPtr pStack, Item nLastOpr);
+
+	Stack aStack;
+	aStack.nTop = -1;								// 초기화
+	push(&aStack, MinOpr);							// -무한대
+	sInfix--;
+	while (*++sInfix) {
+		;
+	}
+	// 수식이 끝났으므로 stack의 연산자를 출력한다.
+	printStackOpr(&aStack, MinOpr);
+	putchar('\n');
+	return true;
+}
+
+int IsStackEmpty(StackPtr pStack)
+{	// 스택이 비었는지를 return 한다.
+	return true;
+}
+
+int IsStackFull(StackPtr pStack)
+{	// 스택이 가득 찼는지를 return 한다.
+	return true;
+}
+
+int push(StackPtr pStack, Item nValue)
+{	// 스택에 nValue를 push하는데, true/false를 return 한다.
+	return true;
+}
+
+Item pop(StackPtr pStack)
+{	// 스택에서 pop하여 return하는데, 스택이 비었으면 EMPTY을 return 한다.
+	return true;
+}
+
+Item top(StackPtr pStack)
+{	// 스택의 꼭대기 원소를 return하는데, 스택이 비었으면 EMPTY을 return 한다.
+	return true;
+}
+
+int printStackOpr(StackPtr pStack, Item nLastOpr)
+{
+	Item nOpr;
+	while ((nOpr = pop(pStack)) != nLastOpr) {
+		if (nOpr == EMPTY)
+			return false;
+		putchar(nOpr);
+	}
+	return true;
+}
+
+int priority(char op, int mode)
+{
+	char precedence[][3] = { 
+		{'^', 3, 4},		// {연산자, ISP, ICP}
+		{'*', 2, 2},
+		{'/', 2, 2},
+		{'+', 1, 1},
+		{'-', 1, 1},
+		{'(', 0, 4},
+		{'?', -1, 0}};
+	int i;
+	int nSize = sizeof(precedence) / sizeof(precedence[0]);
+	for (i = 0; i < nSize; i++)
+		if (op == precedence[i][0])
+			return precedence[i][mode];
+	return -1;
+}
---
+++ MazeBackTrack.cpp
@@ -0,0 +1,150 @@
+#include <stdio.h>
+
+#define M	11
+#define N	15
+#define Size	M * N
+#pragma warning(disable: 4326 4996 6031)
+
+typedef char Maze[M + 2][N + 2];		// Maze 타입을 정의한다.
+
+typedef struct {
+	int nRow;
+	int nCol;
+	int nDir;
+}	Item;
+
+typedef struct  {
+	int nRow;
+	int nCol;
+}	Offsets;
+
+Offsets nMoves[8] = {
+	{-1, 0},	// 북
+	{-1, 1},	// 북동
+	{0, 1},		// 동
+	{1, 1},		// 동남
+	{1, 0},		// 남
+	{1, -1},	// 남서
+	{0, -1},	// 서
+	{-1, -1}};	// 북서
+
+int nTop;
+Item aStack[Size];
+
+Maze maze = {
+	{1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
+	{1, 0, 1, 0, 0, 0, 1, 1, 0, 0, 0, 1, 1, 1, 1, 1, 1},
+	{1, 1, 0, 0, 0, 1, 1, 0, 1, 1, 1, 0, 0, 1, 1, 1, 1},
+	{1, 0, 1, 1, 0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 1, 1, 1},
+	{1, 1, 1, 0, 1, 1, 1, 1, 0, 1, 1, 0, 1, 1, 0, 0, 1},
+	{1, 1, 1, 0, 1, 0, 0, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1},
+	{1, 0, 0, 1, 1, 0, 1, 1, 1, 0, 1, 0, 0, 1, 0, 1, 1},
+	{1, 0, 1, 1, 1, 1, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1},
+	{1, 0, 0, 1, 1, 0, 1, 1, 0, 1, 1, 1, 1, 1, 0, 1, 1},
+	{1, 1, 1, 0, 0, 0, 1, 1, 0, 1, 1, 0, 0, 0, 0, 0, 1},
+	{1, 0, 0, 1, 1, 1, 1, 1, 0, 0, 0, 1, 1, 1, 1, 0, 1},
+	{1, 0, 1, 0, 0, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 0, 1},
+	{1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1}};
+Maze mark;
+
+void main()
+{
+	int FindPath(void);
+	void PrintPath(void);
+
+	for (int i = 0; i < M + 2; i++)
+		for (int j =0; j < N + 2; j++)
+			mark[i][j] = 0;				// 0으로 초기화
+	if (FindPath())						// 길을 찾았다.
+		PrintPath();
+	else								// 길이 없다.
+		printf("No path has been found.\n");
+}
+
+int FindPath()	// 미로에서 길을 찾아라.
+{
+	void Push(Item aPos);
+	int IsEmptyStack();
+	Item Pop(void);
+	void PrintTrial(int nDir, int nRow, int nCol, const char *sMsg);
+
+	Item aPos = {1, 1, 0};
+	mark[1][1] = 1;
+	nTop = -1;
+	Push(aPos);
+	while (IsEmptyStack() == false) {
+		aPos = Pop();
+		PrintTrial(-1, aPos.nRow, aPos.nCol, "<<<Back");
+		while (aPos.nDir < 8) {
+			int nDir = aPos.nDir;
+			int nextRow = aPos.nRow + nMoves[nDir].nRow;
+			int nextCol = aPos.nCol + nMoves[nDir].nCol;
+			if (nextRow == M && nextCol == N) {
+				PrintTrial(nDir, nextRow, nextCol, "Success!!!");
+				Push(aPos);
+				return true;
+			}
+			else if (maze[nextRow][nextCol] == 0 &&
+					 mark[nextRow][nextCol] == 0) {
+				PrintTrial(nDir, nextRow, nextCol, ">>>Go");
+				mark[nextRow][nextCol] = 1;
+				aPos.nDir++;
+				Push(aPos);
+				aPos.nRow = nextRow;
+				aPos.nCol = nextCol;
+				aPos.nDir = 0;
+			}
+			else {
+				PrintTrial(nDir, nextRow, nextCol, 
+					(maze[nextRow][nextCol] == 0) ? "Marked" : "Stop");
+				aPos.nDir++;
+			}
+		}
+	}
+	return false;
+}
+
+void PrintPath()		// 스택에 저장된 길을 출력하라.
+{
+	for (int i = 0; i <= nTop; i++)
+		printf("Position(%d, %d)\n", aStack[i].nRow, aStack[i].nCol);
+	printf("Position(%d, %d)\n\n", M, N);
+}
+
+void PrintTrial(int nDir, int nRow, int nCol, const char *sMsg)
+{
+	static int nTrial;
+	printf("[%03d] ", ++nTrial);
+	for (int i = 0; i <= nTop; i++)
+		putchar((i & 1) ? '|' : 0x20);
+
+	printf("[%c](%d, %d) %s\n", (nDir < 0) ? '-' : nDir + 0x30, nRow, nCol, sMsg);
+}
+
+/*
+ * Stack operations
+ */
+
+int IsEmptyStack()
+{
+	return nTop < 0;
+}
+
+int IsFullStack()
+{
+	return nTop >= Size - 1;
+}
+
+void Push(Item aPos)
+{
+	if (IsFullStack())
+		return;
+	aStack[++nTop] = aPos;
+}
+
+Item Pop()
+{
+	if (IsEmptyStack())
+		return aStack[0];
+	return aStack[nTop--];
+}
